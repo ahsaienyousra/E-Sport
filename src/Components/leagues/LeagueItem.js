db810484a2a72b7ce95ad5f1091e497ca88eed4d
AHSAIEN <yousra.ahsaien@gmail.com>
2020-11-06 23:47:20 +0100

    commit

--- src/Components/leagues/LeagueItem.js
+++ src/Components/leagues/LeagueItem.js
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom';
 
 const LeagueItem = ({league:{id,name,image_url,html_url}})=> {
         return (
-            <div className="card text-center" style={{padding:'5em'}}>
+            <div className="card" style={{padding:'5em'}}>
                 <h3 className="card-header">{name}</h3>
                 <div class="card-body">
-                <img src={image_url} alt='' style={{width:'80px'}}/>
+                <img src={image_url} alt='' style={{width:'100%'}}/>
                 <div>
                     <Link to={`/leagues/${id}`} className="btn btn-dark btn-sm my-1">Details</Link>
                 </div>
