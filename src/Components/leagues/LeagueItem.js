6132686491641f4787c97c44940c4f3ffcf0a284
AHSAIEN <yousra.ahsaien@gmail.com>
2020-11-07 00:01:29 +0100

    commit

--- src/Components/leagues/LeagueItem.js
+++ src/Components/leagues/LeagueItem.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 
 const LeagueItem = ({league:{id,name,image_url,html_url}})=> {
         return (
-            <div className="container-fluid">
-            <div className="card" style={{ backgroundColor: "white"}}>
-                <h3 className="card-header">{name}</h3>
+            <div className="container-fluid text-center">
+            <div className="card"style={{width:'50%'}} >
+                <h3 className="card-header" style={{ backgroundColor: "white"}}>{name}</h3>
                 <div class="card-body">
-                <img src={image_url} alt='' style={{width:'50%'}}/>
+                <img src={image_url ||"https://hearhear.org/wp-content/uploads/2019/09/no-image-icon.png"} alt='' style={{width:'50%'}}/>
                 </div>
-                <div class="card-footer" style={{ backgroundColor: "white"}}>
-                <Link to={`/leagues/${id}`} className="btn btn-dark btn-sm my-1">Details</Link>
+                <div class="card-footer" style={{backgroundColor: "white"}}>
+                <Link to={`/leagues/${id}`} className="btn btn-lg btn-primary btn-sm my-1">Details</Link>
                 </div>
             </div>
             </div>
